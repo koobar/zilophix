94c62558df97129c91f688f6210b1b407d336ef4
koobar <koobar14@gmail.com>
2025-02-03 21:33:34 +0900

    Add SetOnErrorExit function to dll project.

--- zilophixdll/include/zilophixdll.h
+++ zilophixdll/include/zilophixdll.h
@@ -2,11 +2,17 @@
 #define ZILOPHIXDLL_HEADER_INCLUDED
 
 #include "errcode.h"
+#include <stdbool.h>
 
 /*!
  * @brief       Gets last error code.
  * @return      Error code
  */
 error_code GetLastErrorCode();
+
+/*!
+ * @brief               Set on error exit.
+ */
+void SetOnErrorExit(bool onErrorExit);
 
 #endif
--- zilophixdll/zilophixdll.c
+++ zilophixdll/zilophixdll.c
@@ -1,9 +1,9 @@
 #include "./include/zilophixdll.h"
 
-/*!
- * @brief       最後に発生したエラーのエラーコードを取得します。
- * @return      最後に発生したエラーのエラーコード
- */
 error_code GetLastErrorCode() {
     return get_last_error_code();
+}
+
+void SetOnErrorExit(bool onErrorExit) {
+    set_on_error_exit(onErrorExit);
 }
